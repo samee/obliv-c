a36ad0c7007b373f634a0d23c2201beecbf01e52
mdh3hc <mdh3hc@virginia.edu>
2014-06-02 14:32:32 -0400

    Added comments to highest.c

--- test/oblivc/highest/highest.c
+++ test/oblivc/highest/highest.c
@@ -13,28 +13,32 @@ const char* mySide()
 
 int main(int argc,char *argv[])
 {
- 
+  //these variables are used everytime
   ProtocolDesc pd;
   protocolIO io;
+  
+  //checking that the input has the correct number of variables
   if(argc<3)
   { if(argc<2) fprintf(stderr,"Party missing\n");
     else fprintf(stderr,"string missing\n");
     fprintf(stderr,"Usage: %s <1|2> <string>\n",argv[0]);
     return 1;
   }
+  
+  //sets the size of the array
   io.n = argc-2;
- 
-  int g = atoi("15");
+  //the the values in the array to the input values
   int j;
   for(j = 0; j<io.n; j++){
     io.i[j] = atoi(argv[j+2]);
   }
+  //these steps are done everytime to set-up the oc file
   protocolUseStdio(&pd);
   currentParty = (argv[1][0]=='1'?1:2);
   setCurrentParty(&pd,currentParty);
   execYaoProtocol(&pd,highest,&io);
-  fprintf(stderr,"%u\n",yaoGateCount());
   cleanupProtocol(&pd);
+  //prints out the result
   fprintf(stderr,"Result: %d\n",io.res);
   return 0;
 }
