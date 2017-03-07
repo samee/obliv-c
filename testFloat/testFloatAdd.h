9ffdfe6d0e35b50307228b90853cff2f648d0958
Darion Cassel <darion.cassel@gmail.com>
2017-03-06 22:09:15 -0600

    Fix bug in execFloatProtocol (currentProto not properly set)

--- testFloat/testFloatAdd.h
+++ testFloat/testFloatAdd.h
@@ -3,13 +3,10 @@ int cp;
 
 typedef struct {
   float v; // Value
-  int party;
   float ores;
 } protocolIO;
 
 void floatAddi(void* args);
 void load_data(protocolIO *io, float *x, float *y, int party);
 
-
 void printAsBinary(float x);
-// int ocCurrentParty2();
