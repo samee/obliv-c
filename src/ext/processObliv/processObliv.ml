d6b1e85a2ae06fae754a0a06423a6828218c07ff
Weikeng Chen <w.k@berkeley.edu>
2018-04-15 17:18:45 -0700

    fix `processObliv` dropping labels from CIL && ||
    CIL may generate `goto` and labels from && || in some cases (see https://people.eecs.berkeley.edu/~necula/cil/cil004.html).
    
    However, due to an implementation detail, `processObliv` drops the label, causing `gcc`'s compiler error.
    
    This fix corrects the `codegenVisitor` when dealing `statement` with empty list. Such statement cannot be regenerated by the information in the list, and should not be reconstructed.

--- src/ext/processObliv/processObliv.ml
+++ src/ext/processObliv/processObliv.ml
@@ -802,8 +802,10 @@ class codegenVisitor (curFunc : fundec) (dt:depthTracker) (curCond : lval)
     let isDeepVar v = dt#curDepth() = dt#varDepth v in
     match s.skind with
     | Instr ilist -> 
+        let num = List.length ilist in
         let nestedGen = codegenInstr curCond tmpVar isDeepVar in
-        ChangeTo (mkStmt (Instr (mapcat nestedGen ilist)))
+        if num == 0 then DoChildren
+        else ChangeTo (mkStmt (Instr (mapcat nestedGen ilist)))
     | If(c,tb,fb,loc) when isOblivBlock tb ->
         let cond = "__obliv_c__cond" in
         let cv = var (tmpVar ~name:cond oblivBoolType) in
