aee50d478c75686d4f5d87b9921c21c9f55dcfdb
Samee Zahur <sza4uq@virginia.edu>
2018-04-29 22:01:38 -0700

    Slightly simplified label-preservation

--- src/ext/processObliv/processObliv.ml
+++ src/ext/processObliv/processObliv.ml
@@ -802,12 +802,11 @@ class codegenVisitor (curFunc : fundec) (dt:depthTracker) (curCond : lval)
     let isDeepVar v = dt#curDepth() = dt#varDepth v in
     match s.skind with
     | Instr ilist -> 
-        let mkStmt_with_label (sk: stmtkind) (lb: label list) : stmt =
-        { skind = sk; labels = lb; sid = -1; succs = []; preds = [] } in
+        let mkStmtWithLabel (sk: stmtkind) (lb: label list) : stmt =
+          { (mkStmt sk) with labels = lb } in
         let num = List.length ilist in
         let nestedGen = codegenInstr curCond tmpVar isDeepVar in
-        if num == 0 then DoChildren
-        else ChangeTo (mkStmt_with_label (Instr (mapcat nestedGen ilist)) s.labels)
+        ChangeTo (mkStmtWithLabel (Instr (mapcat nestedGen ilist)) s.labels)
     | If(c,tb,fb,loc) when isOblivBlock tb ->
         let cond = "__obliv_c__cond" in
         let cv = var (tmpVar ~name:cond oblivBoolType) in
